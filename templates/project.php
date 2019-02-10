0ce03c80eb5a855493050bc0cc83dbcbdb694573
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-02-10 17:09:15 +0300

    Правит название функции в шаблонах

--- templates/project.php
+++ templates/project.php
@@ -1,4 +1,4 @@
 <li class="main-navigation__list-item">
-    <a class="main-navigation__list-item-link" href="#"><?= filterContent($project); ?></a>
+    <a class="main-navigation__list-item-link" href="#"><?= filter_content($project); ?></a>
     <span class="main-navigation__list-item-count"><?= count_tasks_quantity($tasks, $project); ?></span>
 </li>
