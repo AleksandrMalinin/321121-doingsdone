0aa6f75164d4497dc0ea0f58a778fe9e226a2121
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-02-10 17:11:07 +0300

    Дублирует правки названия функции

--- templates/task.php
+++ templates/task.php
@@ -5,7 +5,7 @@
     <td class="task__select">
         <label class="checkbox task__checkbox">
             <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1" <?php if ($task['done']): ?>checked<?php endif ?>>
-            <span class="checkbox__text"><?= filterContent($task['title']); ?></span>
+            <span class="checkbox__text"><?= filter_content($task['title']); ?></span>
         </label>
     </td>
 
@@ -13,5 +13,5 @@
         <a class="download-link" href="#">Home.psd</a>
     </td>
 
-    <td class="task__date"><?= filterContent($task['date']); ?></td>
+    <td class="task__date"><?= filter_content($task['date']); ?></td>
 </tr>
