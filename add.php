51a22f5a146e2a470865c7b3b9592d3efc5e27d6
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-02-23 16:42:10 +0300

    Добавляет имя юзера и тайтл

--- add.php
+++ add.php
@@ -5,6 +5,9 @@ require_once('./init.php');
 // Текущий юзер
 $user_id = 3;
 
+// Получаем имя текущего пользователя
+$users = get_users_data($connect, $user_id);
+
 // Получаем массив с количеством невыполненных заданий
 $tasks_quantity = get_tasks_quantity_data($connect, $user_id);
 
@@ -13,7 +16,9 @@ $projects = get_projects_data($connect, $user_id, $tasks_quantity);
 
 // Передаём массив с проектами в шаблон
 $page_content = include_template('add.php', [
-    'projects' => $projects
+    'projects' => $projects,
+    'user' => $users['name'],
+    'title' => 'Дела в порядке - Добавление задачи'
 ]);
 
 print($page_content);
