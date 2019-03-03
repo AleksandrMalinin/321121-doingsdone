c8f808f3f3a8b99723d583b90ff9df834184b590
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-03-04 01:19:25 +0300

    Удаляет не нужный js код

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-var $checkbox = document.getElementsByClassName('show_completed');
-
-if ($checkbox.length) {
-  $checkbox[0].addEventListener('change', function (event) {
-    var is_checked = +event.target.checked;
-
-    window.location = '/index.php?show_completed=' + is_checked;
-  });
-}
-
 flatpickr('#date', {
   enableTime: false,
   dateFormat: "d.m.Y",
